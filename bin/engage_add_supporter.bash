aa4f4df373080491959ec0ca5cda239de7e4a669
Allen Leonard <aleonard@salsalabs.com>
2017-08-31 16:23:49 -0500

    Changed API tokens.  Added search by supporterId.

--- bin/engage_add_supporter.bash
+++ bin/engage_add_supporter.bash
@@ -1,20 +1,20 @@
 #!/bin/bash
 curl -H "Content-Type: application/json" \
--H 'authToken: e5AtxX4M8YkcfGpWp8nor4PREygs7KAXgeB5gxSpcwzyhJfNItBcSDZ3ZY900A5HjdJtLg2tCgOoBoRTEUTnWWq-IxPyp_VNdFZGHt6wgjTPJUZQI3bIsN1Nt4gXBGTQ_YbOsk0L7JkreMO0J7mIow' \
+-H 'authToken: ENGAGE-API-TOKEN' \
 -X PUT \
 -d '{ "payload": { "supporters": [ { "contacts": [ {"type":"EMAIL", "value":"aleonard@salsalabs.com", "status":"OPT_IN"} ] } ] }}' \
 https://api.salsalabs.org/api/integration/ext/v1/supporters \
 #| jq -M '.[]'
 
 curl -H "Content-Type: application/json" \
--H 'authToken: e5AtxX4M8YkcfGpWp8nor4PREygs7KAXgeB5gxSpcwzyhJfNItBcSDZ3ZY900A5HjdJtLg2tCgOoBoRTEUTnWWq-IxPyp_VNdFZGHt6wgjTPJUZQI3bIsN1Nt4gXBGTQ_YbOsk0L7JkreMO0J7mIow' \
+-H 'authToken: ENGAGE-API-TOKEN' \
 -X POST \
 -d '{ "payload": { "identifiers": [ "aleonard@salsalabs.com" ], "identifierType": "EMAIL_ADDRESS" }}' \
 https://api.salsalabs.org/api/integration/ext/v1/supporters/search \
 #| jq -M '.[]'
 
 # curl -H "Content-Type: application/json" \
-# -H 'authToken: e5AtxX4M8YkcfGpWp8nor4PREygs7KAXgeB5gxSpcwzyhJfNItBcSDZ3ZY900A5HjdJtLg2tCgOoBoRTEUTnWWq-IxPyp_VNdFZGHt6wgjTPJUZQI3bIsN1Nt4gXBGTQ_YbOsk0L7JkreMO0J7mIow' \
+# -H 'authToken: ENGAGE-API-TOKEN' \
 # -X PUT \
 # -d '{ "payload": { "supporters": [ { "firstName":"Allen","lastName":"Leonard", "contacts": [ {"type":"EMAIL", "value":"aleonard@salsalabs.com", "status":"OPT_OUT"} ] } ] }}' \
 # https://api.salsalabs.org/api/integration/ext/v1/supporters \
--- bin/segment_search.bash
+++ bin/segment_search.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 curl -H "Content-Type: application/json" \
--H 'authToken: Dp5aFQ3HMMz6ARRYQlKGxoeOuG8X7l2N6toEx5o0i7nx31z3Vzq-Oq3DdXYYG6hzY8aFY0lnQGpInF0gIYsSRAQyIIlwYKGdw7uUU4XEyGBfCNO7MCmhS37rsOrWncnKUB0tB6HUgp-QiMcI0wxh2w', \
+-H 'authToken: ENGAGE-API-TOKEN', \
 -X POST \
 -d '{ "payload": { "count":10, "offset":0, "identifiers": ["553e2f43-aa43-497e-9c35-af151ef3abbd"], "identifierType": "SEGMENT_ID"}}' \
 https://api.salsalabs.org/api/integration/ext/v1/segments/search \
