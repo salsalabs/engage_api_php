1fbef90486b25d6ba24020dbab95f03537cfdcba
Allen Leonard <aleonard@salsalabs.com>
2018-07-05 10:44:56 -0500

    Converted to use runtime parameters.

--- src/search_activities.php
+++ src/search_activities.php
@@ -1,20 +1,35 @@
 <?php
-    // Uses Composer.
-    require 'vendor/autoload.php';
-    use GuzzleHttp\Client;
-    
+// Uses Composer.
+require 'vendor/autoload.php';
+use GuzzleHttp\Client;
+use Symfony\Component\Yaml\Yaml;
+
+// Retrieve the runtime parameters and validate them.
+function initialize()
+{
+    $filename = './params/search-activities.yaml';
+    $cred = Yaml::parseFile($filename);
+    if (false == array_key_exists('token', $cred)) {
+        throw new Exception("File " . $filename . " must contain an Engage token.");
+    }
+    return $cred;
+}
+
+function main()
+{
+    $cred = initialize();
     $headers = [
-        'authToken' => 'YOUR-INCREDIBLY-LONG-AUTH-TOKEN-HERE'
-        'Content-Type' => 'application/json'
+        'authToken' => $cred["token"],
+        'Content-Type' => 'application/json',
     ];
     // Payload matches the `curl` bash script.
- 
+
     $payload = [
         'payload' => [
             'modifiedFrom' => '2017-09-01T11:49:24.905Z',
-        	'count' => 10,
-        	'offset' => 0
-        ]
+            'count' => 10,
+            'offset' => 0,
+        ],
     ];
     $method = 'POST';
     $uri = 'https://api.salsalabs.org';
@@ -22,17 +37,19 @@
     $command = '/api/integration/ext/v1/activities/search';
     $client = new GuzzleHttp\Client([
         'base_uri' => $uri,
-        'headers'  => $headers
+        'headers' => $headers,
     ]);
+
     try {
         $response = $client->request($method, $command, [
-            'json'     => $payload
+            'json' => $payload,
         ]);
-        $data = json_decode($response -> getBody());
+        $data = json_decode($response->getBody());
         echo json_encode($data, JSON_PRETTY_PRINT);
     } catch (Exception $e) {
-    echo 'Caught exception: ',  $e->getMessage(), "\n";
-    // var_dump($e);
+        echo 'Caught exception: ', $e->getMessage(), "\n";
+        // var_dump($e);
+    }
 }
 
-?>
+main();
