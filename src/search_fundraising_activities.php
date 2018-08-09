76c1d41b9394f1946dea9db8730a5f116b4c150e
Allen Leonard <aleonard@salsalabs.com>
2018-08-09 17:29:06 -0500

    Demonstrated date range and activity id searches.

--- src/search_fundraising_activities.php
+++ src/search_fundraising_activities.php
@@ -2,37 +2,116 @@
     // Uses Composer.
     require 'vendor/autoload.php';
     use GuzzleHttp\Client;
+    use Symfony\Component\Yaml\Yaml;
     
-    $headers = [
-        'authToken' => 'YOUR-INCREDIBLY-LONG-AUTH-TOKEN-HERE',
-        'Content-Type' => 'application/json'
-    ];
- 
-    $payload = [
-        'payload' => [
-            'modifiedFrom' => '2017-09-01T11:49:24.905Z',
-            'type' => 'FUNDRAISE',
-        	'count' => 10,
-        	'offset' => 0
-        ]
-    ];
-    $method = 'POST';
-    $uri = 'https://api.salsalabs.org';
-    $uri = 'https://hq.uat.igniteaction.net';
-    $command = '/api/integration/ext/v1/activities/search';
-    $client = new GuzzleHttp\Client([
-        'base_uri' => $uri,
-        'headers'  => $headers
-    ]);
-    try {
-        $response = $client->request($method, $command, [
-            'json'     => $payload
-        ]);
-        $data = json_decode($response -> getBody());
-        echo json_encode($data, JSON_PRETTY_PRINT);
-    } catch (Exception $e) {
-    echo 'Caught exception: ',  $e->getMessage(), "\n";
-    // var_dump($e);
-}
+    // App to search for donations in a date range for a particular
+    // donation page.
 
-?>
+    // Config is a YAML file. The Engage API call expects either a 
+    // date range (modifiedFrom and/or modifiedTo) or a list of 
+    // activityIds.  This sameple is equipped with both.  See the
+    // payload comment to learn now to use both types of requests.
+    /*
+    token:          "your-incredibly-long-token"
+    identifierType: FUNDRAISE
+    modifiedFrom:   "2018-07-01T00:00:00.000Z"
+    modifiedTo:     "2018-07-31T23:59:59.999Z"
+    activityIds:
+        - "3a05282d-c648-4c9f-a880-574211b019d6"
+        - "04afe721-ac62-4b2e-aa66-bc6cba7fac71"
+        - "17fd2a78-4ff6-4f3a-b2b1-278355716eff"
+        - "3a05282d-c648-4c9f-a880-574211b019d6"
+        - "48b25138-a021-4eff-89d2-7161d1caed29"
+        - "54ecf33b-b7f7-4d83-85d8-eb9ffac97a66"
+
+    This is a sample of the output:
+
+    Activity ID                          Transaction ID                       Transaction Date         Type      Amount
+    04afe721-ac62-4b2e-aa66-bc6cba7fac71 6f150ba4-f5ad-45cf-a7c6-a6c599a68be0 2018-07-20T02:36:10.002Z CHARGE    50.00
+    17fd2a78-4ff6-4f3a-b2b1-278355716eff 2a48a1cd-3906-46da-bf4f-255e7d338b65 2018-07-15T16:50:54.143Z CHARGE    50.00
+    3a05282d-c648-4c9f-a880-574211b019d6 614e1ea2-9de3-4e9e-b089-b5db0e149463 2018-07-20T02:28:37.025Z CHARGE   100.00
+    48b25138-a021-4eff-89d2-7161d1caed29 6f4e7bed-d504-4507-88a7-73d8ed0bdcf0 2018-07-20T02:23:58.640Z CHARGE    50.00
+    54ecf33b-b7f7-4d83-85d8-eb9ffac97a66 ed3a8e28-7f99-484c-9642-9af68e31a856 2018-07-27T14:38:20.666Z CHARGE    25.00
+                                                                                                       Total    275.00
+
+    */
+
+    // Retrieve the runtime parameters and validate them.
+    function initialize() {
+        $filename = './params/search-fundraising-activities.yaml';
+        $cred =  Yaml::parseFile($filename);
+        if  (FALSE == array_key_exists('token', $cred)) {
+            throw new Exception("File " . $filename . " must contain an Engage token.");
+        }
+        return $cred;
+    }
+    
+    function main()
+    {
+        $cred = initialize();
+        $headers = [
+            'authToken' => $cred["token"],
+            'Content-Type' => 'application/json',
+        ];
+    
+        # This payload defaults to finding donations between two dates.
+        # To see donations for a set of activity IDs
+        #  * comment out modifiedFrom
+        #  * comment out modifiedTo
+        #  * uncomment activityIds
+        #  * add some activity IDs to the param file.
+        $payload = [
+            'payload' => [
+                'type' => $cred["identifierType"],
+                'modifiedFrom' => $cred["modifiedFrom"],
+                'modifiedTo' => $cred["modifiedTo"],
+                #'activityIds' => $cred["activityIds"],
+                'count' => 10,
+                'offset' => 0,
+            ],
+        ];
+
+        $method = 'POST';
+        $uri = 'https://hq.uat.igniteaction.net';
+        $uri = 'https://api.salsalabs.org';
+        $command = '/api/integration/ext/v1/activities/search';
+        $client = new GuzzleHttp\Client([
+            'base_uri' => $uri,
+            'headers' => $headers,
+        ]);
+    
+        try {
+            $response = $client->request($method, $command, [
+                'json' => $payload,
+            ]);
+            $data = json_decode($response->getBody());
+            # echo json_encode($data, JSON_PRETTY_PRINT)."\n";
+
+            $total = 0.00;
+            printf("\n    %-36s %-36s %-24s %-7s %7s\n", "Activity ID", "Transaction ID", "Transaction Date", "Type", "Amount");
+            foreach ( $data -> payload -> activities as $s) {
+                $aid = $s -> activityId;
+                $afn = $s -> activityFormName;
+                $ad = $s -> acivityDate;
+                foreach ($s -> transactions as $t) {
+                    $tt = $t -> type;
+                    if ($tt == "CHARGE") {
+                        $tid = $t -> transactionId;
+                        $td = $t -> date;
+                        $ta = $t -> amount;
+                        $ta = floatval($ta);
+                        $ta = number_format($ta, 2, ".", ",");
+                        printf("    %-36s %-36s %-24s %-7s %7.2f\n", $aid, $tid, $td, $tt, $ta);
+                        $total = $total + $ta;
+                    }
+                }
+            }
+            printf("    %-36s %-36s %-24s %-7s %7.2f\n\n", "", "", "", "Total", $total);
+        } catch (Exception $e) {
+            echo 'Caught exception: ', $e->getMessage(), "\n";
+            // var_dump($e);
+        }
+    }
+    
+    main();
+    
