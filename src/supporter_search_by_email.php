9cd0f8f746456ea64ae7ce18858f95d8a44c0dc4
Allen Leonard <aleonard@salsalabs.com>
2018-10-23 10:44:05 -0500

    Removed non-email-search stuff.

--- src/supporter_search_by_email.php
+++ src/supporter_search_by_email.php
@@ -5,11 +5,12 @@
     use Symfony\Component\Yaml\Yaml;
 
     // App to look up a supporter by email.
-    // Example contents of YAM file.
+    // Example contents of YAML file.
     /*
         identifiers:
             - whatever@domain.com
         token: Your-incredibly-long-Engage-API-token
+        uri: https://api.salsalabs.org/
     */
 
     $filename = './params/supporter-search-by-email.yaml';
@@ -19,61 +20,40 @@
     }
 
     $headers = [
-        'authToken' => $cred['token'],
-        'Content-Type' => 'application/json'
+        'authToken'     => $cred['token'],
+        'Content-Type'  => 'application/json'
     ];
 
     $payload = [ 'payload' => [
-        	'count' => 20,
-        	'offset' => 0,
-            "modifiedFrom" => "2015-05-26T11:49:24.905Z",
+        	'count'             => 20,
+        	'offset'            => 0,
+            'identifiers'       => $cred['identifiers'],
+            'identifierType'    => 'EMAIL_ADDRESS',
         ]
     ];
     $method = 'POST';
-    $uri = 'https://hq.uat.igniteaction.net';
-    $uri = 'https://api.salsalabs.org';
+    $uri = $cred['uri'];
     $command = '/api/integration/ext/v1/supporters/search';
 
     $client = new GuzzleHttp\Client([
-        'base_uri' => $uri,
-        'headers'  => $headers
+        'base_uri'  => $uri,
+        'headers'   => $headers
     ]);
-    $offset = 0;
-    $count = 0;
-    do {
-        try {
-            $payload['payload']["offset"] = $offset;
-            $payload['payload']['count'] = 20;
-            printf("Reading from offset %d\n", $offset);
-            $response = $client->request($method, $command, [
-                'json'     => $payload
-            ]);
+    try {
+        $response = $client->request($method, $command, [
+            'json'     => $payload
+        ]);
 
-            $data = json_decode($response -> getBody());
-            //echo json_encode($data, JSON_PRETTY_PRINT);
-            foreach ( $data -> payload -> supporters as $s) {
-                $c = $s -> contacts[0];
-                $id = $s -> supporterId;
-                if ($s -> result == 'NOT_FOUND') {
-                    $id = $s -> result;
-                }
-                if (isSet($c->longitude) && isSet($c->latitude)) {
-                    printf("%s %-20s %-20s %-25s %s\n",
-                    $id,
-                    $s -> firstName,
-                    $s -> lastName,
-                    $c -> longitude,
-                    $c -> latitude);
-                }
-            }
-            $count = count($data -> payload -> supporters);
-            printf("Reading from offset %d returned %d records\n", $offset, $count);
-            $offset += $count;
-        } catch (Exception $e) {
-            echo 'Caught exception: ',  $e->getMessage(), "\n";
-            break;
+        $data = json_decode($response -> getBody());
+        printf("Results for %d supporters\n", count($data -> payload -> supporters));
+        //echo json_encode($data, JSON_PRETTY_PRINT);
+        foreach ( $data -> payload -> supporters as $s) {
+            $c = $s -> contacts[0];
+            printf("%-40s %s\n",
+                $c -> value,
+                $s -> result);
         }
-    } while ($count > 0);
-    // var_dump($e);
-
+    } catch (Exception $e) {
+        echo 'Caught exception: ',  $e->getMessage(), "\n";
+    }
 ?>
