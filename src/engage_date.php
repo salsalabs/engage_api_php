1228aa411a6ee698d7de50e26461a41f59ef87e7
Allen Leonard <aleonard@salsalabs.com>
2020-04-22 12:21:12 -0500

    Example of formatting Engage date.

--- src/engage_date.php
+++ src/engage_date.php
@@ -2,8 +2,8 @@
 # Return the current date in a form that Engage can grok.
 # Make sure that the value if `$dt` is UTC.
 $dt = new DateTime();
-$format = "Y-m-d H:i:s.U";
-$now = $dt->format($format) . "Z";
+$format = "Y-m-d\TH:i:s.U\Z";
+$now = $dt->format($format);
 echo "Start: ", $now, PHP_EOL;
 ?>
 
