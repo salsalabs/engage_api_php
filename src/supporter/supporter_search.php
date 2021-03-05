c1c8083d530faf58df42e20468fbb867132c119c
Allen Leonard <aleonard@salsalabs.com>
2021-03-05 11:59:10 -0600

    Updated to use per-client YAML file.

--- src/supporter/supporter_search.php
+++ src/supporter/supporter_search.php
@@ -1,84 +1,132 @@
 <?php
-    // Uses Composer.
-    require 'vendor/autoload.php';
-    use GuzzleHttp\Client;
-    use Symfony\Component\Yaml\Yaml;
+// Uses Composer.
+require 'vendor/autoload.php';
+use GuzzleHttp\Client;
+use Symfony\Component\Yaml\Yaml;
 
-    function initialize() {
-        // Read the contents of params/supporter-add.yamlporter-search.yaml.
-        // Example contents:
-        /*         
-            # Using this kind of search.
-            identifierType: EMAIL_ADDRESS
-            # Looking for these supporters.
-            identifiers: 
-            - aleonard@salsalabs.com
-            - barbie@doll.biz
-            - totally@bogus.biz
-            # Engage API token.
-            token: Your-incredibly-long-Engage-token-here
-        */
-        $filename = './params/supporter-search.yaml';
-        $cred =  Yaml::parseFile($filename);
-        if  (FALSE == array_key_exists('token', $cred)) {
-            throw new Exception("File " . $filename . " must contain an Engage token.");
-        }
-        return $cred;
+// Program to retrieve supporter records for a list of identifiers.
+// Engage allows several options for identifiers.  See the options here:
+// https://api.salsalabs.org/help/integration#operation/supporterSearch
+// Most clients will want to use EMAIL_ADDRESS or SUPPORTER_ID.
+//
+// Usage:
+//
+// php src/supporter/supporter_search.php --login YAML_FILE
+//
+// Where:
+//
+//  YAML_FILE is a yaml file containing the info used to retrieve supporters.
+// Here's a sample file.
+/*
+token: your-incredibly-long-engage-integration-token
+identifierType: SUPPORTER_ID
+identifiers:
+- supporterID_1
+- supporterID_2
+- supporterID_3
+ */
+// Engage can retrieve up to a nominal 20 records at a time.  Providing
+// more than that may not work the way tha you want.
+
+// Retrieve the runtime parameters and validate them.
+function initialize()
+{
+    $shortopts = "";
+    $longopts = array(
+        "login:",
+    );
+    $options = getopt($shortopts, $longopts);
+    if (false == array_key_exists('login', $options)) {
+        exit("\nYou must provide a parameter file with --login!\n");
     }
+    $filename = $options['login'];
+    $cred = Yaml::parseFile($filename);
+    validateCredentials($cred, $filename);
+    return $cred;
+}
 
-    // This is the task.  Uses the contents of params/supporter-add.yamlporter-search.yaml to find some
-    // supporters.
-    //
-    // @param array  $cred  Contents of params/supporter-add.yamlporter-search.yaml
-    //
-    function run($cred) {
-        $headers = [
-            'authToken' => $cred['token'],
-        'Content-Type' => 'application/json'
-        ];
-        // 'identifiers' in the YAML file is an array of identifiers.
-        // 'identifierType' is one of the official identifier types.
-        // @see https://help.salsalabs.com/hc/en-us/articles/224470107-Supporter-Data
-        $payload = [
-            'payload' => [
-                'count' => 10,
-                'offset' => 0,
-                'identifiers' => $cred['identifiers'],
-                'identifierType' => $cred['identifierType']
-            ]
-        ];
-        $method = 'POST';
-        $uri = 'https://api.salsalabs.org';
-        $uri = 'https://hq.uat.igniteaction.net';
-        $command = '/api/integration/ext/v1/supporters/search';
-        $client = new GuzzleHttp\Client([
-            'base_uri' => $uri,
-            'headers'  => $headers
-        ]);
-        $response = $client->request($method, $command, [
-            'json'     => $payload
-        ]);
-        $data = json_decode($response -> getBody());
-       // echo json_encode($data, JSON_PRETTY_PRINT);
-        foreach ( $data -> payload -> supporters as $s) {
-            $c = $s -> contacts[0];
-            $id = $s -> supporterId;
-            if ($s -> result == 'NOT_FOUND') {
-                $id = $s -> result;
-            }
-            printf("%s %-20s %-20s %-25s %s\n",
+// Validate the contents of the provided credential file.
+// All fields are required.  Exits on errors.
+function validateCredentials($cred, $filename)
+{
+    $errors = false;
+    $fields = array(
+        "token",
+        "identifierType",
+        "identifiers",
+    );
+    foreach ($fields as $f) {
+        if (false == array_key_exists($f, $cred)) {
+            printf("Error: %s must contain a %s.\n", $filename, $f);
+            $errors = true;
+        }
+    }
+    if ($errors) {
+        exit("Too many errors, terminating.\n");
+    }
+}
+
+// This is the task.  Uses the contents of params/supporter-add.yamlporter-search.yaml to find some
+// supporters.
+//
+// @param array  $cred  Contents of params/supporter-add.yamlporter-search.yaml
+//
+function run($cred)
+{
+    $headers = [
+        'authToken' => $cred['token'],
+        'Content-Type' => 'application/json',
+    ];
+    // 'identifiers' in the YAML file is an array of identifiers.
+    // 'identifierType' is one of the official identifier types.
+    $payload = [
+        'payload' => [
+            'count' => 10,
+            'offset' => 0,
+            'identifiers' => $cred['identifiers'],
+            'identifierType' => $cred['identifierType'],
+        ],
+    ];
+    $method = 'POST';
+    $host = 'https://api.salsalabs.org';
+    $command = '/api/integration/ext/v1/supporters/search';
+    $uri = $host . $command;
+    $client = new GuzzleHttp\Client([
+        'base_uri' => $uri,
+        'headers' => $headers,
+    ]);
+    $response = $client->request($method, $command, [
+        'json' => $payload,
+    ]);
+    $data = json_decode($response->getBody());
+    // echo json_encode($data, JSON_PRETTY_PRINT);
+    foreach ($data->payload->supporters as $s) {
+        $id = $s->supporterId;
+        if ($s->result == 'NOT_FOUND') {
+            $firstName = $s->result;
+            $lastName = "";
+            $email = "";
+            $status = "";
+        } else {
+            $c = $s->contacts[0];
+            $firstName = $s->firstName;
+            $lastName = $s->lastName;
+            $email = $c->value;
+            $status = $c->status;
+        }
+        printf("%s %-20s %-20s %-25s %s\n",
             $id,
-            $s -> firstName,
-            $s -> lastName,
-            $c -> value,
-            $c -> status);
-        }
+            $firstName,
+            $lastName,
+            $email,
+            $status);
     }
+}
 
-    function main() {
-        $cred = initialize();
-        run($cred);
-    }
+function main()
+{
+    $cred = initialize();
+    run($cred);
+}
 
-    main();
-?>
+main();
