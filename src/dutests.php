b265b32ab4d31024515b119b7052f0895dea5fb0
Allen Leonard <aleonard@salsalabs.com>
2021-04-28 10:23:09 -0500

    Refactored test app.

--- src/dutests.php
+++ src/dutests.php
@@ -1,51 +1,68 @@
 <?php
 require_once("demo_utils.php");
 
+function seeJSON($x) {
+    return json_encode($x, JSON_PRETTY_PRINT);
+}
 function see($du, $topic) {
     printf("\n***% s***\n\n", $topic);
     printf("apiHost  is '%s'\n", $du->getAPIHost());
     printf("intToken is '%s'\n", $du->getIntToken());
     printf("devToken is '%s'\n", $du->getWebDevToken());
-    var_dump($du->getEnvironment());
+    printf("environment is\n%s\n", seeJSON($du->getEnvironment()));
+    printf("metrics are\n%s\n", seeJSON($du->getMetrics()));
 }
 
+function pass1(){
 $du = new DemoUtils\DemoUtils();
 $du->setIntToken("Integration Token");
 $du->setWebDevToken("WebDev Token");
 see($du, "Pass 1");
+}
 
+function pass2(){
 $du = new DemoUtils\DemoUtils();
 $du->loadYAML("logins/owc.yaml");
 see($du, "Pass 2");
+}
 
+function pass3(){
 printf("Pass 3, the miscellanous stuff pass");
 printf("Headers, int token\n");
-var_dump($du->getHeaders($du->getIntToken()));
+seeJSON($du->getHeaders($du->getIntToken()));
+}
 
+function pass4(){
 printf("Headers, dev token\n");
-var_dump($du->getHeaders($du->getWebDevToken()));
+seeJSON($du->getHeaders($du->getWebDevToken()));
+}
 
-
+function pass5(){
 printf("Client, int token\n");
-var_dump($du->getClient($du->getIntToken()));
+seeJSON($du->getClient($du->getIntToken()));
+}
 
+function pass6(){
 printf("Client, dev token\n");
-var_dump($du->getClient($du->getWebDevToken()));
+seeJSON($du->getClient($du->getWebDevToken()));
+}
 
-
+function pass7(){
 printf("Client, Int direct\n");
-var_dump($du->getIntClient());
+seeJSON($du->getIntClient());
+}
 
+function pass8() {
 printf("Client, WebDev direct\n");
-var_dump($du->getWebDevClient());
+seeJSON($du->getWebDevClient());
+}
 
-printf("*** Pass 4 ***, command line");
+function pass9() {
 $du = new \DemoUtils\DemoUtils();
-$du->start();
-see($du, "Pass 4");
+$du->appInit();
+see($du, "Pass 9");
+}
 
-printf("*** Passs 4 ***, metrics");
-$metrics = $du->getMetrics();
-$j = json_encode($metrics, JSON_PRETTY_PRINT);
-print($j);
+pass9();
+
 ?>
