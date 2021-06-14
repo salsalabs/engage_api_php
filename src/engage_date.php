2f33304ec8aa5d730d92a88b4930d96807cf5a42
Allen Leonard <aleonard@salsalabs.com>
2021-06-14 14:37:04 -0500

    Fix typo.

--- src/engage_date.php
+++ src/engage_date.php
@@ -1,6 +1,6 @@
 <?php
 # Return the current date in a form that Engage can grok.
-# Make sure that the value if `$dt` is UTC.
+# Make sure that the value of `$dt` is UTC.
 $dt = new DateTime();
 $format = "Y-m-d\TH:i:s.U\Z";
 $now = $dt->format($format);
